a46818388631672fae2df0edf27e3d000fdda6cc
Istoleyourbutter <136261899+AnonCatalyst@users.noreply.github.com>
2024-03-23 22:24:34 -0400

    Update usr.py

--- usr.py
+++ usr.py
@@ -23,10 +23,7 @@ def search_username_on_url(username: str, url: str):
     global visited_urls, visited_html_content
     try:
         if username.lower() not in url.lower():
-            if url.endswith('/'):
-                url += username
-            else:
-                url += '/' + username
+            url += f'/{username}' if url.endswith('/') else f'/{username}'
 
         if url in visited_urls:
             print(f"{Fore.YELLOW}⚠️ {Fore.RED}Skipping duplicate URL: {Fore.WHITE}{url}")
@@ -45,22 +42,16 @@ def search_username_on_url(username: str, url: str):
 
             visited_html_content.add(response.html.raw_html)
 
-            print(
-                f"{Fore.CYAN}🔍 {Fore.BLUE}{username} {Fore.RED}| {Fore.YELLOW}[{Fore.GREEN}✅{Fore.YELLOW}]{Fore.WHITE} URL{Fore.YELLOW}: {Fore.LIGHTGREEN_EX}{url}{Fore.WHITE} {response.status_code}"
-            )
+            print(f"{Fore.CYAN}🔍 {Fore.BLUE}{username} {Fore.RED}| {Fore.YELLOW}[{Fore.GREEN}✅{Fore.YELLOW}]{Fore.WHITE} URL{Fore.YELLOW}: {Fore.LIGHTGREEN_EX}{url}{Fore.WHITE} {response.status_code}")
             # Print HTML content with organized formatting if it's not empty
-            html_content = response.html.raw_html
-            if html_content:
-                print_html(html_content, url)
-            else:
-                print(f"{Fore.YELLOW}HTML Content: {Fore.RED}Empty")
+            print_html(response.html.raw_html, url)
         else:
             # Skip processing for non-200 status codes
             return
     except UnicodeEncodeError:
         logging.error(f"UnicodeEncodeError occurred while printing to console for {username} on {url}")
     except Exception as e:
-        logging.error(f"Error occurred while searching for {username} on {url}: Continueing...")
+        logging.error(f"Error occurred while searching for {username} on {url}: {e}")
 
 
 def print_html(html_content, url):
@@ -69,38 +60,36 @@ def print_html(html_content, url):
             print(f"{Fore.YELLOW}HTML Content for URL {Fore.WHITE}{url}:{Fore.YELLOW} Empty")
             return
 
-        # Convert the HTML content to a string if it's bytes
-        if isinstance(html_content, bytes):
-            html_content = html_content.decode('utf-8')
-
         soup = BeautifulSoup(html_content, 'html.parser')
         if soup:
             title = soup.title.get_text(strip=True) if soup.title else "No title found"
-            print(f"{Fore.YELLOW}🔸 TITLE: {Fore.WHITE}{title}")
-
             meta_description = soup.find("meta", attrs={"name": "description"})
             description = meta_description['content'] if meta_description else "No meta description found"
-            print(f"{Fore.YELLOW}🔸 DESCRIPTION: {Fore.WHITE}{description}")
 
+            print(f"{Fore.YELLOW}{'─' * 40}")
+            print(f"{Fore.YELLOW}🔸 TITLE: {Fore.WHITE}{title}")
+            print(f"{Fore.YELLOW}🔸 DESCRIPTION: {Fore.WHITE}{description}")
             print(f"{Fore.YELLOW}🔸 HTML Content for URL {Fore.WHITE}{url}:{Fore.YELLOW}")
+
+            # Decode bytes to string
+            html_content_str = html_content.decode('utf-8')
             # Print a snippet of the HTML content with line breaks for better readability
             snippet_length = 200  # Adjust as needed
-            html_snippet = html_content[:snippet_length] + ("..." if len(html_content) > snippet_length else "")
+            html_snippet = html_content_str[:snippet_length] + ("..." if len(html_content_str) > snippet_length else "")
             print("\n".join([f"{Fore.CYAN}{line}" for line in html_snippet.split("\n")]))
+            print(f"{Fore.YELLOW}{'─' * 40}")
         else:
             print(f"{Fore.YELLOW}HTML Content for URL {Fore.WHITE}{url}:{Fore.RED} Empty")
     except Exception as e:
         print(f"{Fore.RED}Error occurred while parsing HTML content for URL {Fore.WHITE}{url}:{Fore.RED} {str(e)}")
 
 
-
-
 def main(username):
     with open("src/urls.txt", "r") as f:
         url_list = [x.strip() for x in f.readlines()]
 
-    if username is None:
-        #print("❌ Error: Username cannot be None.")
+    if not username:
+        print("❌ Error: Username cannot be empty.")
         return
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
